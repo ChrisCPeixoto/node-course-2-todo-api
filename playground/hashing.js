15cc34ad813afbe87f0efb846f28795d3c1cb191
Christophe <christophepeixoto@gmail.com>
2018-05-24 10:07:51 +0200

    lots of change

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,39 +1,38 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
-var data ={
+var data = {
   id: 10
-}
+};
 
-var token=jwt.sign(data,'123abc');
+var token = jwt.sign(data, '123abc');
 console.log(token);
 
-var decoded=jwt.verify(token,'123abc');
-console.log(decoded);
-//jwt.verify
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
 
-// var message ='I am user number 3';
+// var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
 // console.log(`Message: ${message}`);
 // console.log(`Hash: ${hash}`);
-//
-// var data ={
+
+// var data = {
 //   id: 4
-// }
-//
-// var token ={
+// };
+// var token = {
 //   data,
-//   hash:SHA256(JSON.stringify(data)  + 'somesecret').toString()
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
 // }
 //
-// token.data.id=5;
-// token.hash=SHA256(JSON.stringify(token.data)).toString();
 //
-// var resultHash = SHA256(JSON.stringify(token.data)+ 'somesecret').toString();
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
 //
-// if(resultHash ===token.hash){
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// if (resultHash === token.hash) {
 //   console.log('Data was not changed');
-// }else{
-//   console.log('Data was changed.')
+// } else {
+//   console.log('Data was changed. Do not trust!');
 // }
--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,18 +1,20 @@
 const {ObjectID} = require('mongodb');
 
-const mongoose = require('./../server/db/mongoose');
+const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
-const {User} =  require('./../server/models/user');
+const {User} = require('./../server/models/user');
 
-//Todo.remove({})
-
-// Todo.remove({}).then((result)=>{
+// Todo.remove({}).then((result) => {
 //   console.log(result);
 // });
 
-// Todo.findOneAndRemove({})
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
 
+// Todo.findOneAndRemove({_id: '57c4610dbb35fcbf6fda1154'}).then((todo) => {
+//
+// });
 
-Todo.findByIdAndRemove('5b041124d20fab9c2a74cbe8').then((todo)=>{
+Todo.findByIdAndRemove('57c4610dbb35fcbf6fda1154').then((todo) => {
   console.log(todo);
 });
